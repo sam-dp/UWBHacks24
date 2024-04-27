eb4c3a7fee88ea006d9691b2abe3cfa8fcdbcc43
sam-dp <cloudysaga@gmail.com>
2024-04-26 22:20:46 -0700

    Working out how to pass image to server

--- root/11-resources/05-js/main.js
+++ root/11-resources/05-js/main.js
@@ -2,6 +2,7 @@
 // const https = require('https');
 
 const uploadInput = document.getElementById("input-file");
+const image = document.getElementById("image");
 
 // User Uploads Image
 // Client-side JS passes the image URL to Server-side (fetch)
@@ -10,16 +11,18 @@ uploadInput.onchange = function()
     // Capture uploaded image
     console.log("in uploadInput.onchange()");
     imageURL = URL.createObjectURL(uploadInput.files[0]);
+    image.src = imageURL;
+
     console.log(imageURL);
     
     // Pass image to server
-    fetch('/upload')
-    .then(response => response.json())
-    .then(imageURL => {
-        console.log();
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    // fetch('/upload')
+    // .then(response => response.json())
+    // .then(imageURL => {
+    //     console.log();
+    // })
+    // .catch(error => {
+    //     console.error('Error:', error);
+    // });
 
 }
